adfe023e3c55f2e9d8eeb2ba9f2e80e369ac38e4
James Player <james@spingload.co.nz>
2012-04-30 17:09:00 +1200

    added js files

---
+++ jquery.biglink.js
@@ -0,0 +1,43 @@
+/*
+ * jquery.bigLink.js
+ * 
+ */
+
+(function($) {
+    $.fn.bigLink = function(options) {
+        var settings = {
+            target    : 'a',
+            avoid     : 'a'
+        };
+        
+        return this.each(function(){
+            if (options){
+                $.extend(settings, options);
+            }
+            
+            var stop = false;
+            
+            $(this).addClass('bigLink');
+            
+            $(this).find(settings.avoid).click(function(e){
+                stop = true;
+            });          
+            
+            $(this).click(function(e){
+                        
+                var url = $(this).find(settings.target).attr('href');     
+                
+                if (typeof(url) != 'undefined' && !stop){
+                    if (e.metaKey){
+                        window.open(url);
+                    } else {
+                        window.location = url; 
+                    }
+                }
+            });
+            
+            
+            
+        });
+    };
+})( jQuery );
