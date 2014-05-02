554ff554e5f792b8007cf5eb50d72824deba300a
James Player <james.player@gmail.com>
2014-05-02 12:34:58 +1200

    updated readme to use markdown, fixed bug that stopped bigLink from working after an 'avoid' link was clicked

--- jquery.biglink.js
+++ jquery.biglink.js
@@ -1,6 +1,6 @@
 /*
  * jquery.bigLink.js
- * 
+ *
  */
 
 (function($) {
@@ -9,35 +9,38 @@
             target    : 'a',
             avoid     : 'a'
         };
-        
+
         return this.each(function(){
             if (options){
                 $.extend(settings, options);
             }
-            
+
             var stop = false;
-            
+
             $(this).addClass('bigLink');
-            
+
             $(this).find(settings.avoid).click(function(e){
                 stop = true;
-            });          
-            
+            });
+
             $(this).click(function(e){
-                        
-                var url = $(this).find(settings.target).attr('href');     
-                
+
+                var url = $(this).find(settings.target).attr('href');
+
                 if (typeof(url) != 'undefined' && !stop){
                     if (e.metaKey){
                         window.open(url);
                     } else {
-                        window.location = url; 
+                        window.location = url;
                     }
                 }
+
+                stop = false;
+
             });
-            
-            
-            
+
+
+
         });
     };
 })( jQuery );
